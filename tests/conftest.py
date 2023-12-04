6ea736e7c06284ec743f6336dc2d00b0c26d27c4
Adrián Cuesta <adriancuestariejos@gmail.com>
2023-12-04 19:57:38 +0100

    Fix isort conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,12 +5,7 @@ from httpx import BasicAuth
 import pytest
 
 from app.controller import create_api
-from app.utils.common import (
-    BlockchainConfig,
-    Environment,
-    get_blockchain_config,
-    load_config,
-)
+from app.utils.common import BlockchainConfig, Environment, get_blockchain_config, load_config
 
 
 @pytest.fixture()
